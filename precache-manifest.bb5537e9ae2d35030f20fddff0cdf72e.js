84ea1de1ee948f41472a857f74cd06b3f8d1fc06
Kevin Garvey <kmgarvey279@gmail.com>
2019-08-07 18:31:34 -0700

    Updates

---
+++ precache-manifest.bb5537e9ae2d35030f20fddff0cdf72e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "796cf8a63f63cdb15001bd0e6aa6dbe5",
+    "url": "/Helpful-Human-Challenge/index.html"
+  },
+  {
+    "revision": "92754a50934a02b54892",
+    "url": "/Helpful-Human-Challenge/static/css/main.26951b97.chunk.css"
+  },
+  {
+    "revision": "ebe22e0791352a94b0b3",
+    "url": "/Helpful-Human-Challenge/static/js/2.5fca4cbf.chunk.js"
+  },
+  {
+    "revision": "92754a50934a02b54892",
+    "url": "/Helpful-Human-Challenge/static/js/main.8bb3c3ac.chunk.js"
+  },
+  {
+    "revision": "791a242d090bbffd54ba",
+    "url": "/Helpful-Human-Challenge/static/js/runtime~main.1042e0d7.js"
+  },
+  {
+    "revision": "629f0661643c5026c42d228e37026f08",
+    "url": "/Helpful-Human-Challenge/static/media/logo-symbol.629f0661.svg"
+  }
+]);
